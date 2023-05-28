ea317aaf2815ce3310c98d0a1d827e88a179cd56
nksazonov <nsazonov@openware.com>
2023-05-29 01:18:14 +0300

    add connector

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core';
+
+import { hooks as metaMaskHooks, metaMask } from './connectors/metaMask'
+import { MetaMask } from '@web3-react/metamask';
+import MetaMaskCard from './components/connectorCards/MetaMaskCard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, metaMaskHooks]];
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider connectors={connectors}>
+      <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+        <MetaMaskCard />
+      </div>
+    </Web3ReactProvider>
   </React.StrictMode>
 );
