52ab8914f85fd96bc32965ab4ea3bc2e658573a8
nksazonov <nsazonov@openware.com>
2023-06-11 21:02:37 +0300

    add adapters

---
+++ website/src/data/adapters/patientAdapters.ts
@@ -0,0 +1,13 @@
+import { BloodTypesMap, DiagnosisMap } from "../maps";
+
+export function dateToAge(date: number): number {
+  return Math.floor((Date.now() / 1000 - date) / 60 / 60 / 24 / 365);
+}
+
+export function bloodTypeToText(bloodType: number): string {
+  return BloodTypesMap.get(bloodType) || "Unknown";
+}
+
+export function diagnosisIdToText(diagnosisId: number): string {
+  return DiagnosisMap.get(diagnosisId) || "Unknown";
+}
