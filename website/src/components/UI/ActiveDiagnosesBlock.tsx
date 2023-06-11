52c2e862642253b94743485842824cd82a87529f
nksazonov <nsazonov@openware.com>
2023-06-11 21:17:21 +0300

    ActiveDiagnosesBlock

---
+++ website/src/components/UI/ActiveDiagnosesBlock.tsx
@@ -0,0 +1,21 @@
+import { ActiveDiagnoses } from "../../data/types";
+
+interface Props {
+  activeDiagnoses: ActiveDiagnoses;
+  className?: string;
+}
+
+function ActiveDiagnosesBlock({activeDiagnoses, className}: Props) {
+  return (
+     <div className={`bg-blue-light px-14 py-10 ${className || ''}`}>
+      <h2 className="text-4xl mb-16 text-center">Active diagnoses</h2>
+      <ul className="flex flex-col gap-6 text-3xl list-disc list-inside">
+        {activeDiagnoses.map(diagnosis => (
+          <li>{diagnosis}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ActiveDiagnosesBlock;
