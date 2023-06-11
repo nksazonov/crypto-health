a5acedee0a13e113f139cbedf8c87e9499965218
nksazonov <nsazonov@openware.com>
2023-06-11 12:30:49 +0300

    add Header component

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -5,7 +5,8 @@ import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core';
 
 import { hooks as metaMaskHooks, metaMask } from './connectors/metaMask'
 import { MetaMask } from '@web3-react/metamask';
-import MetaMaskCard from './components/connectorCards/MetaMaskCard';
+import MetaMaskCard from './components/wallet/connectorCards/MetaMaskCard';
+import MainPage from './pages/MainPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,9 +17,10 @@ const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, metaMaskHooks]];
 root.render(
   <React.StrictMode>
     <Web3ReactProvider connectors={connectors}>
-      <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+      {/* <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
         <MetaMaskCard />
-      </div>
+      </div> */}
+      <MainPage />
     </Web3ReactProvider>
   </React.StrictMode>
 );
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#71BCF9',
+        'blue-light': '#F1F8FE',
+        'blue-dark': '#1E3A8A',
+        'blue-medium': '#D8ECFD',
+      }
+    },
   },
   plugins: [],
 }
