7e05ea6bbe1b754178fceb9478427943af8368cb
nksazonov <nsazonov@openware.com>
2023-06-12 11:17:44 +0300

    add edit button

--- website/src/components/UI/PatientGeneralInfoBlock.tsx
+++ website/src/components/UI/PatientGeneralInfoBlock.tsx
@@ -1,16 +1,21 @@
 import { bloodTypeToText } from "../../data/adapters/patientAdapters";
 import { timestampToAge } from "../../data/adapters/timeAdapters";
 import { PatientGeneralInfo } from "../../data/types";
+import Button from "./Button";
 
 interface Props {
   patient: PatientGeneralInfo,
+  onEditClick?: () => void,
   className?: string,
 }
 
-function PatientGeneralInfoBlock({ patient, className }: Props) {
+function PatientGeneralInfoBlock({ patient, onEditClick, className }: Props) {
   return (
     <div className={`bg-blue-light px-14 py-10 ${className || ''}`}>
-      <h2 className="text-4xl mb-16 text-center">General information</h2>
+      <div className="flex items-center justify-center mb-16">
+        <h2 className="text-4xl text-center">General information</h2>
+        {onEditClick && <Button text="Edit" onClick={onEditClick} className="h-8 border-2 rounded-lg ml-4 px-2 py-0 text-lg hover:text-blue" />}
+      </div>
       <div className="grid grid-cols-2 grid-rows-6 gap-x-12 gap-y-6 text-3xl">
         <span>Name:</span> <span>{patient.name}</span>
         <span>Surname:</span> <span>{patient.surname}</span>
