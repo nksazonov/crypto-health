a5a482672f15a455d6cd76726c3c7c92a627ee90
nksazonov <nsazonov@openware.com>
2023-06-14 23:16:30 +0300

    add toISODateString adapters

--- website/src/data/adapters/timeAdapters.ts
+++ website/src/data/adapters/timeAdapters.ts
@@ -5,3 +5,11 @@ export function timestampToAge(date: number): number {
 export function timestampToDateString(timestamp: number) {
   return new Date(timestamp * 1000).toLocaleDateString();
 }
+
+export function timestampToISODateString(timestamp: number) {
+  return new Date(timestamp * 1000).toISOString().slice(0, 16);
+}
+
+export function nowToISODateString() {
+  return new Date().toISOString().slice(0, 16);
+}
