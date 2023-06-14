8655008213f6bd7ef06dc34346a8083cae73649b
nksazonov <nsazonov@openware.com>
2023-06-15 00:13:36 +0300

    change placeholder text color

--- website/src/components/UI/Input.tsx
+++ website/src/components/UI/Input.tsx
@@ -15,7 +15,7 @@ function Input({ value, type, onChange, placeholder, className }: Props) {
       placeholder={placeholder || "Search..."}
       value={value}
       onChange={onChange}
-      className={`h-16 w-96 border-blue border-4 p-4 text-xl text-blue outline-0 hover:bg-blue-light focus:bg-blue focus:text-white placeholder:text-blue focus:placeholder:text-white ${className || ''}`}
+      className={`h-16 w-96 border-blue border-4 p-4 text-xl text-blue outline-0 hover:bg-blue-light focus:bg-blue focus:text-white placeholder:text-blue-medium focus:placeholder:text-blue-medium ${className || ''}`}
     />
   )
 }
