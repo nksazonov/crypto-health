8e48622d7992e5bf2af10b9d75176e6c78259c94
nksazonov <nsazonov@openware.com>
2023-06-18 00:08:43 +0300

    fix remove MainPage blue bg

--- website/src/pages/MainPage.tsx
+++ website/src/pages/MainPage.tsx
@@ -4,7 +4,7 @@ import Button from '../components/UI/Button';
 
 function MainPage() {
   return (
-    <div className='flex flex-col w-full items-center justify-center h-5/6 bg-blue-light'>
+    <div className='flex flex-col w-full items-center justify-center h-5/6'>
       <HighlightedText text="Connect Wallet" className='bg-blue mb-6 py-6' />
       <span className='text-3xl font-medium text-blue mb-32'>to continue as a Patient, Doctor or Admin</span>
       <Button text="Connect Wallet" />
