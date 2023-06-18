1596b7b4e12ea6f1242d78fb8a0a40ae3754df41
nksazonov <nsazonov@openware.com>
2023-06-18 04:48:03 +0300

    get, set patient + info, diagnoses

--- website/src/hooks/useCryptoHealth.ts
+++ website/src/hooks/useCryptoHealth.ts
@@ -3,7 +3,7 @@ import useDApp from "./useDApp";
 import { ethers } from "ethers";
 import CryptoHealthArtifacts from "../artifacts/CryptoHealth.json";
 import { config } from "../config/config";
-import { CryptoHealth } from "../artifacts/CryptoHealth";
+import { CryptoHealth, IHealth } from "../artifacts/CryptoHealth";
 import {
   parseActiveDiagnoses,
   parseDiagnosesHistory,
@@ -50,32 +50,98 @@ export default function useCryptoHealth() {
     return role;
   }, [account, CryptoHealthContract]);
 
-  const getPatientInfo = React.useCallback(async () => {
-    if (!account) return null;
-    if (CryptoHealthContract() === null) return null;
+  const getPatientInfo = React.useCallback(
+    async (patientAddress: string) => {
+      if (CryptoHealthContract() === null) return null;
 
-    const CryptoHealth = CryptoHealthContract()!;
-    const patientInfo = await CryptoHealth.getPatient(account);
-    return parsePatientInfo(patientInfo);
-  }, [account, CryptoHealthContract]);
+      const CryptoHealth = CryptoHealthContract()!;
+      const patientInfo = await CryptoHealth.getPatient(patientAddress);
+      return parsePatientInfo(patientInfo);
+    },
+    [CryptoHealthContract]
+  );
 
-  const getActiveDiagnoses = React.useCallback(async () => {
-    if (!account) return null;
-    if (CryptoHealthContract() === null) return null;
+  const getActiveDiagnoses = React.useCallback(
+    async (patientAddress: string) => {
+      if (CryptoHealthContract() === null) return null;
 
-    const CryptoHealth = CryptoHealthContract()!;
-    const activeDiagnoses = await CryptoHealth.getActiveDiagnoses(account);
-    return parseActiveDiagnoses(activeDiagnoses);
-  }, [account, CryptoHealthContract]);
+      const CryptoHealth = CryptoHealthContract()!;
+      const activeDiagnoses = await CryptoHealth.getActiveDiagnoses(
+        patientAddress
+      );
+      return parseActiveDiagnoses(activeDiagnoses);
+    },
+    [CryptoHealthContract]
+  );
 
-  const getDiagnosesHistory = React.useCallback(async () => {
-    if (!account) return null;
-    if (CryptoHealthContract() === null) return null;
+  const getDiagnosesHistory = React.useCallback(
+    async (patientAddress: string) => {
+      if (CryptoHealthContract() === null) return null;
 
-    const CryptoHealth = CryptoHealthContract()!;
-    const diagnosesHistory = await CryptoHealth.getDiagnosesHistory(account);
-    return parseDiagnosesHistory(diagnosesHistory);
-  }, [account, CryptoHealthContract]);
+      const CryptoHealth = CryptoHealthContract()!;
+      const diagnosesHistory = await CryptoHealth.getDiagnosesHistory(
+        patientAddress
+      );
+      return parseDiagnosesHistory(diagnosesHistory);
+    },
+    [CryptoHealthContract]
+  );
+
+  const addPatient = React.useCallback(
+    async (patientAddress: string, patient: IHealth.PatientStruct) => {
+      if (!account) return null;
+      if (CryptoHealthContract() === null) return null;
+
+      const CryptoHealth = CryptoHealthContract()!;
+      const tx = await CryptoHealth.addPatient(patientAddress, patient);
+      await tx.wait();
+    },
+    [account, CryptoHealthContract]
+  );
+
+  const updatePatient = React.useCallback(
+    async (patientAddress: string, patient: IHealth.PatientStruct) => {
+      if (!account) return null;
+      if (CryptoHealthContract() === null) return null;
+
+      const CryptoHealth = CryptoHealthContract()!;
+      const tx = await CryptoHealth.updatePatient(patientAddress, patient);
+      await tx.wait();
+    },
+    [account, CryptoHealthContract]
+  );
+
+  const deletePatient = React.useCallback(
+    async (patientAddress: string) => {
+      if (!account) return null;
+      if (CryptoHealthContract() === null) return null;
+
+      const CryptoHealth = CryptoHealthContract()!;
+      const tx = await CryptoHealth.deletePatient(patientAddress);
+      await tx.wait();
+    },
+    [account, CryptoHealthContract]
+  );
+
+  const addDiagnosisRecord = React.useCallback(
+    async (
+      patientAddress: string,
+      diagnosisCode: number,
+      isDiagnosisActive: boolean
+    ) => {
+      if (!account) return null;
+      if (CryptoHealthContract() === null) return null;
+
+      const CryptoHealth = CryptoHealthContract()!;
+      const tx = await CryptoHealth.addDiagnosisRecord(
+        patientAddress,
+        diagnosisCode,
+        isDiagnosisActive
+      );
+      await tx.wait();
+    },
+    [account, CryptoHealthContract]
+  );
 
   return {
     CryptoHealthContract,
@@ -83,5 +149,9 @@ export default function useCryptoHealth() {
     getPatientInfo,
     getActiveDiagnoses,
     getDiagnosesHistory,
+    addPatient,
+    updatePatient,
+    deletePatient,
+    addDiagnosisRecord,
   };
 }
