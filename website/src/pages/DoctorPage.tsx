a56d5038c052ea7287ca13f77d3a9ddf834ce23e
nksazonov <nsazonov@openware.com>
2023-06-18 04:52:49 +0300

    DoctorPage logic done

--- website/src/pages/DoctorPage.tsx
+++ website/src/pages/DoctorPage.tsx
@@ -1,24 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/UI/Button";
 import HighlightedText from "../components/UI/HighlightedText";
 import Input from "../components/UI/Input";
 import ActiveDiagnosesBlock from "../components/UI/ActiveDiagnosesBlock";
 import DiagnosesHistoryBlock from "../components/UI/DiagnosesHistoryBlock";
 import PatientGeneralInfoBlock from "../components/UI/PatientGeneralInfoBlock";
-import { activeDiagnoses, diagnosesHistory as diagnosesHistoryMockup, patient as patientMockup } from "../data/mockup";
 import DialogueWindowLayout from "../components/UI/DialogueWindowLayout";
 import EditGeneralInfoBlock from "../components/UI/EditGeneralInfoBlock";
-import { Diagnosis, PatientGeneralInfo } from "../data/types";
+import { ActiveDiagnoses, DiagnosesHistory, Diagnosis, PatientGeneralInfo } from "../data/types";
 import AddDiagnosisBlock from "../components/UI/AddDiagnosisBlock";
 import AddPatientBlock from "../components/UI/AddPatientBlock";
+import useDApp from "../hooks/useDApp";
+import {ethers} from 'ethers';
+import useCryptoHealth from "../hooks/useCryptoHealth";
+import { patientGeneralInfo2Struct } from "../data/adapters/patientAdapters";
 
 function DoctorPage() {
+  const {account, disconnectWallet} = useDApp();
+  const {getPatientInfo, getActiveDiagnoses, getDiagnosesHistory, addPatient, updatePatient, addDiagnosisRecord} = useCryptoHealth();
+
   const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const [patient, setPatient] = useState<PatientGeneralInfo>(patientMockup);
-  const [diagnosesHistory, setDiagnosesHistory] = useState<Diagnosis[]>(diagnosesHistoryMockup);
+  useEffect(() => {
+    if (!ethers.utils.isAddress(searchQuery)) return;
 
-  const [isPatientPresent, setIsPatientPresent] = useState<boolean>(false);
+
+  }, [searchQuery]);
+
+  const [patient, setPatient] = useState<PatientGeneralInfo | null>(null);
+  const [activeDiagnoses, setActiveDiagnoses] = useState<ActiveDiagnoses | null>(null);
+  const [diagnosesHistory, setDiagnosesHistory] = useState<DiagnosesHistory | null>(null);
+
+  useEffect(() => {
+    if (!ethers.utils.isAddress(searchQuery)) {
+      setPatient(null);
+      setActiveDiagnoses(null);
+      setDiagnosesHistory(null);
+      return;
+    };
+
+    getPatientInfo(searchQuery)
+      .then((info) => {
+        setPatient(info!);
+      })
+      .catch((e) =>{
+        console.log(e);
+        setPatient(null);
+      });
+  }, [account, searchQuery, getPatientInfo]);
+
+  useEffect(() => {
+    if (!ethers.utils.isAddress(searchQuery)) return;
+
+
+    getActiveDiagnoses(searchQuery)
+      .then((diagnoses) => {
+        setActiveDiagnoses(diagnoses!);
+      })
+      .catch((e) => {
+        console.log(e);
+        setActiveDiagnoses(null);
+      });
+  }, [account, searchQuery, getActiveDiagnoses]);
+
+  useEffect(() => {
+    if (!ethers.utils.isAddress(searchQuery)) return;
+
+    getDiagnosesHistory(searchQuery)
+      .then((history) => {
+        setDiagnosesHistory(history!);
+      })
+      .catch((e) => {
+        console.log(e);
+        setDiagnosesHistory(null);
+      });
+  }, [account, searchQuery, getDiagnosesHistory]);
+
   const [isEditingPatient, setIsEditingPatient] = useState<boolean>(false);
   const [isAddingDiagnosis, setIsAddingDiagnosis] = useState<boolean>(false);
   const [isAddingPatient, setIsAddingPatient] = useState<boolean>(false);
@@ -27,15 +84,27 @@ function DoctorPage() {
     setSearchQuery(e.target.value);
   }
 
-  const addDiagnosis = (diagnosis: Diagnosis) => {
-    setDiagnosesHistory([diagnosis, ...diagnosesHistory]);
+  const handleAddPatient = (patient: PatientGeneralInfo) => {
+    addPatient(searchQuery, patientGeneralInfo2Struct(patient));
+
+    setPatient(patient);
+    setIsAddingPatient(false);
   }
 
-  const addPatient = (patient: PatientGeneralInfo) => {
-    setPatient(patient);
-    setIsPatientPresent(true);
+  const handleUpdatePatient = (patient: PatientGeneralInfo) => {
+    updatePatient(searchQuery, patientGeneralInfo2Struct(patient));
 
-    console.log("send TX about adding patient")
+    setPatient(patient);
+    setIsEditingPatient(false)
+  }
+
+  const handleAddDiagnosis = (diagnosis: Diagnosis) => {
+    if (!diagnosesHistory) return;
+
+    addDiagnosisRecord(searchQuery, diagnosis.id, diagnosis.isActive);
+
+    setDiagnosesHistory([diagnosis, ...diagnosesHistory]);
+    setIsAddingDiagnosis(false)
   }
 
   return (
@@ -44,17 +113,17 @@ function DoctorPage() {
         <div className="flex items-center w-full">
           <HighlightedText text="Doctor" />
           <Input value={searchQuery} onChange={handleSearch} className="ml-16 w-2/5" />
-          <Button text="Add a patient" disabled={isPatientPresent || searchQuery === ''} onClick={() => setIsAddingPatient(true)} className="ml-16 h-16" />
+          <Button text="Add a patient" disabled={!!patient || !ethers.utils.isAddress(searchQuery)} onClick={() => setIsAddingPatient(true)} className="ml-16 h-16" />
         </div>
-        <Button text="Disconnect" negative />
+        <Button text="Disconnect" onClick={() => disconnectWallet()} negative />
       </div>
 
-      {isPatientPresent
+      {patient
         ?
           <div className="grow mt-14 flex justify-between">
             <PatientGeneralInfoBlock patient={patient} onEditClick={() => setIsEditingPatient(true)} className="" />
-            <ActiveDiagnosesBlock activeDiagnoses={activeDiagnoses} className="" />
-            <DiagnosesHistoryBlock diagnosesHistory={diagnosesHistory} onAddClick={() => setIsAddingDiagnosis(true)} className="" />
+            {activeDiagnoses && <ActiveDiagnosesBlock activeDiagnoses={activeDiagnoses} className="" />}
+            {diagnosesHistory && <DiagnosesHistoryBlock diagnosesHistory={diagnosesHistory} onAddClick={() => setIsAddingDiagnosis(true)} className="" />}
           </div>
         : searchQuery === ''
           ? <></>
@@ -62,16 +131,16 @@ function DoctorPage() {
       }
 
       {
-        isEditingPatient &&
+        isEditingPatient && patient &&
         <DialogueWindowLayout>
-          <EditGeneralInfoBlock heading='Change general info' patient={patient} onConfirmClick={(patient: PatientGeneralInfo) => {setPatient(patient); setIsEditingPatient(false)}} onCancelClick={() => setIsEditingPatient(false)} />
+          <EditGeneralInfoBlock heading='Change general info' patient={patient} onConfirmClick={(patient: PatientGeneralInfo) => handleUpdatePatient(patient)} onCancelClick={() => setIsEditingPatient(false)} />
         </DialogueWindowLayout>
       }
 
       {
         isAddingDiagnosis &&
         <DialogueWindowLayout>
-          <AddDiagnosisBlock heading='Add Diagnosis' onConfirmClick={(diagnosis: Diagnosis) => {addDiagnosis(diagnosis); setIsAddingDiagnosis(false)}} onCancelClick={() => setIsAddingDiagnosis(false)} />
+          <AddDiagnosisBlock heading='Add Diagnosis' onConfirmClick={(diagnosis: Diagnosis) => handleAddDiagnosis(diagnosis)} onCancelClick={() => setIsAddingDiagnosis(false)} />
         </DialogueWindowLayout>
       }
 
@@ -79,7 +148,7 @@ function DoctorPage() {
       {
         isAddingPatient &&
         <DialogueWindowLayout>
-          <AddPatientBlock heading='Add a patient' onConfirmClick={(patient: PatientGeneralInfo) => {addPatient(patient); setIsAddingPatient(false)}} onCancelClick={() => setIsAddingPatient(false)} />
+          <AddPatientBlock heading='Add a patient' onConfirmClick={(patient: PatientGeneralInfo) => handleAddPatient(patient)} onCancelClick={() => setIsAddingPatient(false)} />
         </DialogueWindowLayout>
       }
 
